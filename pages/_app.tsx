7f64136445f1cdbaccdfa60396b342aa76f4218f
Anish Agnihotri <contact@anishagnihotri.com>
2021-10-09 15:10:07 -0400

    Working version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "styles/global.scss"; // Global styles
+import { providers } from "ethers"; // Ethers provider
+import type { AppProps } from "next/app"; // Types
+import { Web3ReactProvider } from "@web3-react/core"; // Web3React
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+/**
+ * Returns instantiated Ethers provider
+ * @param provider from Web3React
+ * @returns {providers.Web3Provider} ethers provider
+ */
+function getLibrary(provider: any): providers.Web3Provider {
+  return new providers.Web3Provider(provider);
 }
-export default MyApp
+
+export default function FlashbotsRPC({ Component, pageProps }: AppProps) {
+  return (
+    // Wrap site in Web3 Provider
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  );
+}
